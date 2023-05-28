52a65b7ac77d161cb63fb8f3f7d0ac5a8805d661
Jubair Ahmed <jubairahmed3000@gmail.com>
2023-05-28 12:36:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const mongoose = require("mongoose");
+ 
+const app = express();
+const UserModel = require("./models/Users");
+const cors = require('cors');
+
+
+mongoose.connect("mongodb+srv://jubairshaikh9999:EGD2YLGeuRqNUqgf@love.zup5jmy.mongodb.net/?retryWrites=true&w=majority")
+
+app.use(cors());
+app.use(express.json())
+
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hi There");
+});
+
+app.get("/getUsers", async (req, res) => {
+    try {
+      const users = await UserModel.find({});
+      res.json(users);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+  
+app.post("/createUser", async (req, res) => {
+     const user = req.body
+     const newUser = new UserModel(user);
+     await newUser.save();
+     res.json(user)
+});
+  
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+// create a Good Password 
+// MongoCompass 
+// copy strigId in Compass  
+// open in terminal ----
---
+++ models/Users.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    age:{
+        type:Number,
+        required:true,
+    },
+    username:{
+        type:String,
+        required: true,
+    },
+})
+
+
+const UserModel = mongoose.model("db",UserSchema)
+
+
+module.exports = UserModel
+
+
+// db == Name of the dataBase 
